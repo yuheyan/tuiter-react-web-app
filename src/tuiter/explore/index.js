d4137743dd988c265868eedb8613afa925a61793
yuheyan <yyh9695@163.com>
2022-10-20 13:53:06 -0700

    A6 Tuiter modified href

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -18,13 +18,13 @@ const ExploreComponent = () => {
             </div>
             <ul className="nav nav-pills mb-2">
                 <li className="nav-item">
-                    <a href={'#'} className="nav-link active">For You</a>
+                    <a href={"/#"} className="nav-link active">For You</a>
                 </li>
                 <li className="nav-item">
-                    <a href={'#'} className="nav-link">Trending</a>
+                    <a href={"/#"} className="nav-link">Trending</a>
                 </li>
                 <li className="nav-item">
-                    <a href={'#'} className="nav-link">News</a>
+                    <a href={"/#"} className="nav-link">News</a>
                 </li>
             </ul>
             <div className="position-relative mb-2">
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -3,36 +3,36 @@ import React from "react";
 const NavigationSidebar = ({active = 'explore'}) => {
     return (
         <div className="list-group">
-            <a href={'#'} className="list-group-item">Tuiter</a>
-            <a href={'#'} className={`list-group-item
+            <a href={"/#"} className="list-group-item">Tuiter</a>
+            <a href={"/#"} className={`list-group-item
                     ${active === 'home'?'active':''}`}>
                 Home
             </a>
-            <a href={'#'} className={`list-group-item
+            <a href={"/#"} className={`list-group-item
                     ${active === 'explore'?'active':''}`}>
                 Explore
             </a>
-            <a href={'#'} className={`list-group-item
+            <a href={"/#"} className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
                 Notifications
             </a>
-            <a href={'#'} className={`list-group-item
+            <a href={"/#"} className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
                 Messages
             </a>
-            <a href={'#'} className={`list-group-item
+            <a href={"/#"} className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
                 Bookmarks
             </a>
-            <a href={'#'} className={`list-group-item
+            <a href={"/#"} className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
                 Lists
             </a>
-            <a href={'#'} className={`list-group-item
+            <a href={"/#"} className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
                 Profile
             </a>
-            <a href={'#'} className={`list-group-item
+            <a href={"/#"} className={`list-group-item
                     ${active === 'more'?'active':''}`}>
                 More
             </a>
