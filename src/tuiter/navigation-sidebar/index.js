0c89a851831f35e8e5932f44afd050224f40a734
yuheyan <yyh9695@163.com>
2022-10-20 13:47:47 -0700

    A6 Tuiter modified

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -3,36 +3,36 @@ import React from "react";
 const NavigationSidebar = ({active = 'explore'}) => {
     return (
         <div className="list-group">
-            <a className="list-group-item">Tuiter</a>
-            <a className={`list-group-item
+            <a href={'#'} className="list-group-item">Tuiter</a>
+            <a href={'#'} className={`list-group-item
                     ${active === 'home'?'active':''}`}>
                 Home
             </a>
-            <a className={`list-group-item
+            <a href={'#'} className={`list-group-item
                     ${active === 'explore'?'active':''}`}>
                 Explore
             </a>
-            <a className={`list-group-item
+            <a href={'#'} className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
                 Notifications
             </a>
-            <a className={`list-group-item
+            <a href={'#'} className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
                 Messages
             </a>
-            <a className={`list-group-item
+            <a href={'#'} className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
                 Bookmarks
             </a>
-            <a className={`list-group-item
+            <a href={'#'} className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
                 Lists
             </a>
-            <a className={`list-group-item
+            <a href={'#'} className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
                 Profile
             </a>
-            <a className={`list-group-item
+            <a href={'#'} className={`list-group-item
                     ${active === 'more'?'active':''}`}>
                 More
             </a>
