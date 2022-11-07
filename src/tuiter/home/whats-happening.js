e431f00e22e2aacb56f616fa17312a083aae3ab9
yuheyan <yyh9695@163.com>
2022-11-06 21:01:54 -0800

    A7 img add alt

--- src/tuiter/home/whats-happening.js
+++ src/tuiter/home/whats-happening.js
@@ -14,7 +14,7 @@ const WhatsHappening = () => {
     return (
         <div className="row">
             <div className="col-auto">
-                <img src="/images/nasa.jpg" width={60}/>
+                <img alt={""} src="/images/nasa.jpg" width={60}/>
             </div>
             <div className="col-10">
        <textarea value={whatsHappening} placeholder="What's happening?"
